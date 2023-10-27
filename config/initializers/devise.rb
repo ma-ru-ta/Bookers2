f79fb18a976b6236b0b20e3a7a7c825a5cc26c74
EC2 Default User <ec2-user@ip-172-31-19-133.ap-northeast-1.compute.internal>
2023-10-27 14:52:39 +0000

    [finish]books_controller

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,7 +14,7 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
-  # config.secret_key = 'f469081ebc63454b4a8cfcc31d606e17a8a48b5fbdcb56e388b97f5d57900b6a1936553ee200fca0af98088480edae6ab3f2afc27c654d660c282872e7971c7a'
+  # config.secret_key = '400998f7f1c0fcefaa1b8525337e5e6140aaf244c32de12a3224dff1b8a04b33d847d320c9a2f072f46c55f9915818fed962683a08beae6d9ba301222968e35a'
 
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
@@ -126,7 +126,7 @@ Devise.setup do |config|
   config.stretches = Rails.env.test? ? 1 : 12
 
   # Set up a pepper to generate the hashed password.
-  # config.pepper = '9bbd759375203541be6e61357a89a516a1317f3873326e9a6ea933cc08f9f27a72c85a840c81f0e939b823bb9a9d541ffcc66d89e6bba0be37ad3d2e1816379e'
+  # config.pepper = '6828f515b81b39d79840f9211759ef650b976bb02a224db34c3a5ab1bbd76c8821aadfab5241c8bc4aaa6a615410111043eadbd7408e498e309c6018d2495130'
 
   # Send a notification to the original email when the user's email is changed.
   # config.send_email_changed_notification = false
